8b00da4e1b3cfb5b67d704e10cb3343484988cb4
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2022-03-09 12:49:42 +0800

    Add files via upload

--- Pset1/GravityCalculator.java
+++ Pset1/GravityCalculator.java
@@ -1,1 +1,18 @@
 
+public class GravityCalculator {
+	public static void main(String [] arguments) {
+		double gravity = -9.81; // Earth's gravity in m/s^2
+		double initialVelocity = 0.0;
+		double failingTime = 10.0;
+		double initialPosition = 0.0;
+		double finalPosition = 0.5 * (gravity *(failingTime * failingTime)) + (initialVelocity * failingTime) + initialPosition;
+		System.out.println("The object's position after " + failingTime + " second is " + finalPosition + " m." );
+		
+	}
+}
+
+// The output of the unmodified program is:
+// "The object's position after 10.0 second is 0.0 m."
+
+// The output of the modified program is:
+// "The object's position after 10.0 second is -490.5 m."
